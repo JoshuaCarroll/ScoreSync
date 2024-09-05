e627b42eeadace46317c711c2d8b75527599f544
Joshua Carroll <JoshuaCarroll@users.noreply.github.com>
2024-09-04 21:44:31 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.IO.Ports;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using ScoreSync;
+using System.Net.Sockets;
+
+class Program
+{
+    static Dictionary<Regex, Action<Match, ScoreboardOCRData>> regexHandlers;
+    static string EndPointUrl;
+    static ScoreboardOCRData scoreboardData = new ScoreboardOCRData();
+
+    static async Task Main(string[] args)
+    {
+        // Check if the correct number of command line arguments is passed
+        if (args.Length != 3)
+        {
+            Console.WriteLine("Usage: ScoreSync <COM port> <serverAddress> <serverPort>");
+            return;
+        }
+
+        string portName = args[0];
+        string EndPointUrl = args[1];
+        int EndPointPort;
+
+        if (!int.TryParse(args[2], out EndPointPort))
+        {
+            Console.WriteLine("Usage: ScoreSync <COM port> <serverAddress> <serverPort>");
+            Console.WriteLine("  Server port must be numeric.");
+            return;
+        }
+
+        InitializeRegexHandlers();
+        
+        int baudRate = 9600;
+        Parity parity = Parity.None;
+        int dataBits = 8;
+        StopBits stopBits = StopBits.One;
+
+        // Check if the specified COM port is available
+        if (!Array.Exists(SerialPort.GetPortNames(), port => port.Equals(portName, StringComparison.OrdinalIgnoreCase)))
+        {
+            Console.WriteLine($"COM port {portName} is not available.");
+            return;
+        }
+
+        using (SerialPort serialPort = new SerialPort(portName, baudRate, parity, dataBits, stopBits))
+        {
+            serialPort.Open();
+            Console.WriteLine("Waiting for data...");
+
+            while (true)
+            {
+                string data = ReadData(serialPort);
+                if (!string.IsNullOrEmpty(data))
+                {
+                    if (TransformToScoreboardData(data))
+                    {
+                        string jsonData = scoreboardData.ToJson();
+                        await SendJsonData(jsonData, EndPointPort, EndPointUrl);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"|{data}|");
+                    }
+                }
+            }
+        }
+    }
+
+    static void InitializeRegexHandlers()
+    {
+        regexHandlers = new Dictionary<Regex, Action<Match, ScoreboardOCRData>>
+        {
+            {
+                new Regex(@"F(.{10})(.{2})(.{1})(.{10})(.{2})(.{1})(.{1})(.{2})(.{2})(.{1})"),
+                (match, data) =>
+                {
+                    data.ScoreHome = match.Groups[2].Value;
+                    data.ScoreAway = match.Groups[5].Value;
+                    data.TimeoutsHome = match.Groups[3].Value;
+                    data.TimeoutsAway = match.Groups[6].Value;
+                    data.Downs = match.Groups[7].Value;
+                    data.Yards = match.Groups[8].Value;
+                    data.LOS = match.Groups[9].Value;
+                    data.Possession = match.Groups[10].Value;
+                }
+            },
+            {
+                new Regex(@"C(.{5})(.{1})(.{2})"),
+                (match, data) =>
+                {
+                    data.GameClock = $"{match.Groups[1].Value.Substring(0, 2)}:{match.Groups[1].Value.Substring(2, 2)}.{match.Groups[1].Value.Substring(4, 1)}";
+                    data.Period = match.Groups[2].Value;
+                    data.ShotClock = match.Groups[3].Value;
+                }
+            }
+        };
+    }
+
+    static string ReadData(SerialPort serialPort)
+    {
+        StringBuilder dataBuffer = new StringBuilder();
+        bool inFrame = false;
+
+        while (true)
+        {
+            try
+            {
+                int byteRead = serialPort.ReadByte();
+
+                if (byteRead == 0x02) // STX character
+                {
+                    inFrame = true;
+                    dataBuffer.Clear();
+                }
+                else if (byteRead == 0x03) // ETX character
+                {
+                    inFrame = false;
+                    break;
+                }
+                else if (inFrame)
+                {
+                    dataBuffer.Append((char)byteRead);
+                }
+            }
+            catch (OperationCanceledException ex)
+            {
+                throw ex;
+            }
+        }
+
+        return dataBuffer.ToString();
+    }
+
+    static bool TransformToScoreboardData(string data)
+    {
+        foreach (var regexHandler in regexHandlers)
+        {
+            Match match = regexHandler.Key.Match(data);
+            if (match.Success)
+            {
+                regexHandler.Value(match, scoreboardData);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static async Task SendJsonData(string serverAddress, int port, string jsonData)
+    {
+        try
+        {
+            using (TcpClient client = new TcpClient(serverAddress, port))
+            using (NetworkStream networkStream = client.GetStream())
+            {
+                byte[] data = Encoding.UTF8.GetBytes(jsonData);
+                await networkStream.WriteAsync(data, 0, data.Length);
+                Console.Write(".");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Failed to send data over TCP. Exception: {ex.Message}\r\n{ex.StackTrace}");
+            Console.WriteLine(jsonData);
+        }
+    }
+}
---
+++ ScoreboardOCRData.cs
@@ -0,0 +1,248 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreSync
+{
+    class ScoreboardOCRData
+    {
+        public string GameClock;
+        public string Period
+        {
+            get
+            {
+                return period;
+            }
+            set
+            {
+                period = checkNumericValue(value);
+            }
+        }
+        public string Quarter
+        {
+            get
+            {
+                return period;
+            }
+            set
+            {
+                period = checkNumericValue(value);
+            }
+        }
+        public string ShotClock
+        {
+            get
+            {
+                return shotClock;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    shotClock = "";
+                }
+                else
+                {
+                    shotClock = value.Trim();
+                }
+            }
+        }
+        public string PlayClock
+        {
+            get
+            {
+                return shotClock;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    shotClock = "";
+                }
+                else
+                {
+                    shotClock = value.Trim();
+                }
+            }
+        }
+        public string ScoreAway
+        {
+            get
+            {
+                return scoreAway;
+            }
+            set
+            {
+                scoreAway = checkNumericValue(value);
+            }
+        }
+        public string ScoreHome
+        {
+            get
+            {
+                return scoreHome;
+            }
+            set
+            {
+                scoreHome = checkNumericValue(value);
+            }
+        }
+        public string FoulsAway
+        {
+            get
+            {
+                return foulsAway;
+            }
+            set
+            {
+                foulsAway = checkNumericValue(value);
+            }
+        }
+        public string FoulsHome
+        {
+            get
+            {
+                return foulsHome;
+            }
+            set
+            {
+                foulsHome = checkNumericValue(value);
+            }
+        }
+        public string TimeoutsAway
+        {
+            get
+            {
+                return timeoutsAway;
+            }
+            set
+            {
+                timeoutsAway = checkNumericValue(value);
+            }
+        }
+        public string TimeoutsHome
+        {
+            get
+            {
+                return timeoutsHome;
+            }
+            set
+            {
+                timeoutsHome = checkNumericValue(value);
+            }
+        }
+        public string Downs
+        {
+            get
+            {
+                return down;
+            }
+            set
+            {
+                down = checkNumericValue(value);
+            }
+        }
+        public string Yards
+        {
+            get
+            {
+                return toGo;
+            }
+            set
+            {
+                toGo = checkNumericValue(value);
+            }
+        }
+        public string LOS
+        {
+            get
+            {
+                return ballOn;
+            }
+            set
+            {
+                ballOn = checkNumericValue(value);
+            }
+        }
+        public string Possession
+        {
+            get
+            {
+                return possession;
+            }
+            set
+            {
+                possession = value;
+            }
+        }
+        public string PossessionAway
+        {
+            get
+            {
+                if (Possession == "V")
+                {
+                    return "1";
+                }
+                else
+                {
+                    return "";
+                }
+            }
+        }
+        public string PossessionHome
+        {
+            get
+            {
+                if (Possession == "H")
+                {
+                    return "1";
+                }
+                else
+                {
+                    return "";
+                }
+            }
+        }
+
+        private string period;
+        private string scoreAway;
+        private string scoreHome;
+        private string foulsAway;
+        private string foulsHome;
+        private string timeoutsAway;
+        private string timeoutsHome;
+        private string shotClock;
+        private string down;
+        private string toGo;
+        private string ballOn;
+        private string possession;
+
+        public string ToJson()
+        {
+            string json = JsonConvert.SerializeObject(this, Formatting.None);
+            json = "{\"type\":\"ocr\",\"values\":" + json + "}\n";
+            Debug.WriteLine(json);
+            return json;
+        }
+
+        private string checkNumericValue(string val)
+        {
+            int i;
+
+            if ((val == null) || (val.Trim() == ""))
+            {
+                val = "0";
+            }
+            else if (!int.TryParse(val, out i))
+            {
+                val = "0";
+            }
+
+            return val;
+        }
+    }
+}
